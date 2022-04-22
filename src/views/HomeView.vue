213f4533185a70d6f5e2316fc8cef1c506788e74
Vic <syuan0212@gmail.com>
2022-04-22 17:43:05 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <p class="text-primary">
+    Home
+  </p>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+const { resolve } = require('path');
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }
+  },
+})
